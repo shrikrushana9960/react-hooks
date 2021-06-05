701a463b9ddd97dde6ff3f03d4752209b5725570
shri <85093480+shrikrushana9960@users.noreply.github.com>
2021-06-04 23:57:04 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { useState,useEffect } from 'react';
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+const storeUserInformation = localStorage.getItem('IsLogIn');
+useEffect(()=>{
+if (storeUserInformation === "1") setIsLoggedIn(true);
+else setIsLoggedIn(false);
+},[storeUserInformation]);
+
+    
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    const storeUserInformation=localStorage.setItem('IsLogIn','1');
+
+    
+
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
